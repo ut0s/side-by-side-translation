7510f3cdd4bb2b50798198fe1d20ebf6b964806a
Yuto TAGASHIRA <ut0s.dev@gmail.com>
2023-09-16 18:20:07 +0900

    chore: remove comment

--- src/content-script/index.ts
+++ src/content-script/index.ts
@@ -29,14 +29,11 @@ chrome.runtime.onMessage.addListener(async function (message, sender, sendRespon
 
 function split() {
   // wrap src document
-  // flex-fill
   $("body").children().wrapAll("<div id='side_by_side_src' class='pane' translate='no'></div>")
   $("body").append("<div id='side_by_side_dst'  class='pane' translate='yes'></div>")
 
   // TODO rename id to unique
   $("#side_by_side_src").children().clone().appendTo("#side_by_side_dst")
-  // $("body").children().wrapAll("<div class='d-inline-flex'></div>")
-  // $("body").children().wrapAll("<div id='side_by_side_container' class='d-flex flex-row'></div>")
   $("body").children().wrapAll("<div id='side_by_side_container' class=''></div>")
 }
 
